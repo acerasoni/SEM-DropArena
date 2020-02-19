2f4ef99a5d79dff73337841b992e2a5a00ebf9f4
psyjb10 <psyjb10@exmail.nottingham.ac.uk>
2020-02-19 19:02:22 +0000

    code003/instantiate-chaser: J, Create Instantiate script for Chaser

--- DropArena/Assets/Scripts/Instantiate.cs
+++ DropArena/Assets/Scripts/Instantiate.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class Instantiate : MonoBehaviour
 {
+    public GameObject Chaser;
+    public GameObject Cube;
+    Vector3 Pos;
     // Start is called before the first frame update
     void Start()
-    {
+    {   
+        while (GameObject.FindGameObjectsWithTag("chaser").Length <= 2)
+        {
+            Create();
+        }
         
     }
 
@@ -14,5 +21,13 @@ public class Instantiate : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void Create(){
+        Pos = new Vector3(4f, 0, 4f);
+		GameObject Chaser = (GameObject)Instantiate( Cube, Pos, transform.rotation);
+		Chaser.name = "chaser";
+        Chaser.tag = "chaser";
+		
     }
 }
