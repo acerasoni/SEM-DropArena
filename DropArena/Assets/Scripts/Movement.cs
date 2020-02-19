84ca20b842122b3f2cd7f39ebbc60e50ed422f3a
Andrea Giulio Cerasoni <32521086+acerasoni@users.noreply.github.com>
2020-02-19 21:22:44 +0000

    code004: Enhancing movement

--- DropArena/Assets/Scripts/Movement.cs
+++ DropArena/Assets/Scripts/Movement.cs
@@ -25,13 +25,13 @@ public class Movement : MonoBehaviour
              //This does the trick: Input.GetAxis("HorizontalPlayer1")
              Player1.transform.position = new Vector3(Player1.transform.position.x + Input.GetAxis("HorizontalPlayer1"), Player1.transform.position.y, Player1.transform.position.z);
              //Processing Player1 input for vertical 
-             Player1.transform.position = new Vector3(Player1.transform.position.x, Player1.transform.position.y + Input.GetAxis("VerticalPlayer1"), Player1.transform.position.z);
+             Player1.transform.position = new Vector3(Player1.transform.position.x, Player1.transform.position.y, Player1.transform.position.z + Input.GetAxis("VerticalPlayer1"));
  
              //Processing Player2 input for horizontal 
              //This does the trick: Input.GetAxis("HorizontalPlayer2")
              Player2.transform.position = new Vector3(Player2.transform.position.x + Input.GetAxis("HorizontalPlayer2"), Player2.transform.position.y, Player2.transform.position.z);
              //Processing Player2 input for vertical 
-             Player2.transform.position = new Vector3(Player2.transform.position.x, Player2.transform.position.y + Input.GetAxis("VerticalPlayer2"), Player2.transform.position.z);
+             Player2.transform.position = new Vector3(Player2.transform.position.x, Player2.transform.position.y, Player2.transform.position.z + Input.GetAxis("VerticalPlayer2"));
          }
      }
  }
