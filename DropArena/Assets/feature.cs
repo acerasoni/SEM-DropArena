0a2fea0287f9c64c43881d2aea6ffd802b24421a
a7324678 <alyxx4@nottingham.ac.uk>
2020-02-25 16:34:30 +0000

    feature of chaser

---
+++ DropArena/Assets/feature.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feature : MonoBehaviour
+{
+    public float movementSpeed = 6.0f;
+    private Vector3 horizontalMovement;
+    private Vector3 verticalMovement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMovement = Input.GetAxis("Horizontal") * Vector3.right * movementSpeed;
+        verticalMovement = Input.GetAxis("Vertical") * Vector3.forward * movementSpeed;
+        Vector3 movement = horizontalMovement + verticalMovement;
+        
+    }
+}
