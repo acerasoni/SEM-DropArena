91fd068bd077e228032af46497097443dfde9180
Andrea Giulio Cerasoni <32521086+acerasoni@users.noreply.github.com>
2020-02-25 12:40:58 +0000

    refactor002: Make player variables public

--- DropArena/Assets/Scripts/Movement.cs
+++ DropArena/Assets/Scripts/Movement.cs
@@ -6,9 +6,10 @@ public class Movement : MonoBehaviour
 {
     //Player Objects
      [SerializeField]
-     private GameObject _player1;
+     public GameObject player1;
      [SerializeField]
-     private GameObject _player2;
+     public GameObject player2;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,19 +20,19 @@ public class Movement : MonoBehaviour
     void Update()
     {
         //Check if players are assigned
-         if(_player1 && _player1)
+         if(player1 && player1)
          {
              //Processing Player1 input for horizontal 
              //This does the trick: Input.GetAxis("HorizontalPlayer1")
-             _player1.transform.position = new Vector3(_player1.transform.position.x + Input.GetAxis("HorizontalPlayer1"), _player1.transform.position.y, _player1.transform.position.z);
+             player1.transform.position = new Vector3(player1.transform.position.x + Input.GetAxis("HorizontalPlayer1"), player1.transform.position.y, player1.transform.position.z);
              //Processing Player1 input for vertical 
-             _player1.transform.position = new Vector3(_player1.transform.position.x, _player1.transform.position.y, _player1.transform.position.z + Input.GetAxis("VerticalPlayer1"));
+             player1.transform.position = new Vector3(player1.transform.position.x, player1.transform.position.y, player1.transform.position.z + Input.GetAxis("VerticalPlayer1"));
  
              //Processing Player2 input for horizontal 
              //This does the trick: Input.GetAxis("HorizontalPlayer2")
-             _player2.transform.position = new Vector3(_player2.transform.position.x + Input.GetAxis("HorizontalPlayer2"), _player2.transform.position.y, _player2.transform.position.z);
+             player2.transform.position = new Vector3(player2.transform.position.x + Input.GetAxis("HorizontalPlayer2"), player2.transform.position.y, player2.transform.position.z);
              //Processing Player2 input for vertical 
-             _player2.transform.position = new Vector3(_player2.transform.position.x, _player2.transform.position.y, _player2.transform.position.z + Input.GetAxis("VerticalPlayer2"));
+             player2.transform.position = new Vector3(player2.transform.position.x, player2.transform.position.y, player2.transform.position.z + Input.GetAxis("VerticalPlayer2"));
          }
      }
  }
