a457823a837d1387482b5037b85abc41ed7d0e27
Andrea Giulio Cerasoni <32521086+acerasoni@users.noreply.github.com>
2020-03-03 10:24:57 +0000

    bugfix013: A&D&X, Adding InstantiateGem script and assigning it to gem

--- DropArena/Assets/Scripts/Instantiate.cs
+++ DropArena/Assets/Scripts/Instantiate.cs
@@ -30,9 +30,8 @@ public class Instantiate : MonoBehaviour
         GameObject.Find("_player1").GetComponent<PlayerGem>().retrieveChaser();
         GameObject.Find("_player2").GetComponent<PlayerGem>().retrieveChaser();
 
-        _gemPos = generateRandomPosition();
+        _gem.AddComponent<InstantiateGem>();
 		_gem = GameObject.CreatePrimitive(PrimitiveType.Cube); 
-        _gem.transform.position = _gemPos;
 		_gem.name = "gem";
         _gem.GetComponent<Renderer>().material = gemMaterial;
         _gem.transform.localScale = new Vector3(0.25f, 0.25f, 0.25f);
@@ -46,39 +45,6 @@ public class Instantiate : MonoBehaviour
     void Update()
     {
 
-    }
-    
-    // Generates random position on one of the 4 edges.
-    // The size of the base is 10, but we will set the square position to 9 to avoid collision with edge obstacles.
-    public Vector3 generateRandomPosition() {
-        Vector3 gPos;
-
-        System.Random rand = new System.Random();
-        int range = 4;
-        float rFloat = (float) (rand.NextDouble()* range);
-
-        // Determine axis
-        if(rand.NextDouble() >= 0.5) {
-            // Determine left or right
-            if(rand.NextDouble() >= 0.5){
-                // Left
-                gPos = new Vector3(0f, 0.49f, rFloat);
-            } else {
-                // Right
-                gPos = new Vector3(8.5f, 0.49f, rFloat);
-            }
-        } else {
-            // Determine top or bottom
-            if(rand.NextDouble() >= 0.5) {
-                // Top
-                 gPos = new Vector3(rFloat, 0.49f, 8.5f);
-            } else {
-                // Bottom
-                 gPos = new Vector3(rFloat, 0.49f, 0f);
-            }
-        }
-        
-        return gPos;
     }
   
 }
