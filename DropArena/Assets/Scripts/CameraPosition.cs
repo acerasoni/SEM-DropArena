c3ace4b2fcc6024c730e4bd748f548e11340418f
psyjb10 <psyjb10@exmail.nottingham.ac.uk>
2020-03-04 17:01:21 +0000

    gs018/map-transitions: J, reordered build scenes and added scene map transitions

---
+++ DropArena/Assets/Scripts/CameraPosition.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class CameraPosition : MonoBehaviour
+
+{
+    private static float _time = 3f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _time -= Time.deltaTime;
+         if (_time < 0)
+         {
+            GameObject.Find("Main Camera").transform.position = new Vector3(3.95f, 7.3f, -5.5f);             
+         }  
+    }
+}
