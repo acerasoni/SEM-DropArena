be89ba9f804a1828ef2d0a66a0fe5b424b3fd58b
Andrea Giulio Cerasoni <32521086+acerasoni@users.noreply.github.com>
2020-03-04 06:17:52 +0000

    code013: Switching movement type from force.impulse to transform

--- DropArena/Assets/Scripts/Movement.cs
+++ DropArena/Assets/Scripts/Movement.cs
@@ -2,93 +2,93 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour
-{
-    //Player Objects
-     [SerializeField]
-     public GameObject player1;
-     [SerializeField]
-     public GameObject player2;
-     level lvl = new level();
-     public float movementBonusPlayer1;
-     public float movementBonusPlayer2;
+public class Movement : MonoBehaviour {
 
-     public bool freezePlayer1;
-     public bool freezePlayer2;
+    level lvl = new level ();
 
-     public float nudgeBonusPlayer1;
-     public float nudgeBonusPlayer2;
+    // Keyboard controls
+    public string horizontalAxis, verticalAxis;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        movementBonusPlayer1 = 1;
-           movementBonusPlayer2 = 1;
+    // Movement state variables
+    private float movementBonus;
+    private float nudgeBonus;
+    private bool freeze;
 
-           freezePlayer1 = false;
-           freezePlayer2 = false;
+    private Rigidbody _playerBody;
 
-           nudgeBonusPlayer1 = 0;
-           nudgeBonusPlayer2 = 0;
+    void Start () {
+
+        // Initialise movement state
+        movementBonus = 1;
+        freeze = false;
+        nudgeBonus = 0;
+        _playerBody = this.GetComponent<Rigidbody> ();
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        //Check if players are assigned
-         if(player1 && player1)
-         {
-             Rigidbody player1Body = player1.GetComponent<Rigidbody>();
-             Rigidbody player2Body = player2.GetComponent<Rigidbody>();
-             
-            if(!freezePlayer1) {
-             //Processing Player1 input for horizontal 
-             player1.transform.position = new Vector3(player1.transform.position.x + Input.GetAxis("HorizontalPlayer1") * movementBonusPlayer1, 
-             player1.transform.position.y, player1.transform.position.z);
-             //Processing Player1 input for vertical 
-             player1.transform.position = new Vector3(player1.transform.position.x, player1.transform.position.y, 
-             player1.transform.position.z + Input.GetAxis("VerticalPlayer1") * movementBonusPlayer1);
-            } 
-            if(!freezePlayer2) {
-             //Processing Player2 input for horizontal 
-             player2.transform.position = new Vector3(player2.transform.position.x + Input.GetAxis("HorizontalPlayer2") * movementBonusPlayer1, 
-             player2.transform.position.y, player2.transform.position.z);
-             //Processing Player2 input for vertical 
-             player2.transform.position = new Vector3(player2.transform.position.x, player2.transform.position.y, 
-             player2.transform.position.z + Input.GetAxis("VerticalPlayer2") * movementBonusPlayer1);
-            }
-        
-            nudgePlayer(player1Body, nudgeBonusPlayer1);
-            nudgePlayer(player2Body, nudgeBonusPlayer2);
-         }
+    void Update () {
 
-         if (player1.transform.position.y < 0)
-         {
-             lvl.levelloader();
-         }
+        // Move the player via the keyboard commands
+        movePlayer ();
+        // Nudge the player inward if they are close to the arena
+        nudgePlayer ();
+        // Check if the player has fallen
+        checkIfHasFallen ();
 
-        if (player2.transform.position.y < 0)
-         {
-             lvl.levelloader();
-         }
-     }
+    }
 
-     void nudgePlayer(Rigidbody playerBody, float nudgeBonus) {
-          // Check if close to the edge, in which case give a little nudge inward
-        if(playerBody.position.x < 0.1f) {
+    void movePlayer () {
+        if (!freeze) {
+            //Processing horizontal input
+            this.transform.position = new Vector3 (this.transform.position.x + Input.GetAxis (horizontalAxis) * 0.35f * movementBonus,
+                this.transform.position.y, this.transform.position.z);
+            //Processing vertical input
+            this.transform.position = new Vector3 (this.transform.position.x, this.transform.position.y,
+                this.transform.position.z + Input.GetAxis (verticalAxis) * 0.35f * movementBonus);
+        }
+    }
+
+    void nudgePlayer () {
+        // Check if close to the edge, in which case give a little nudge inward
+        if (_playerBody.position.x < 0.1f) {
             // If on the left edge
-             playerBody.AddForce(0.04f + nudgeBonus, 0, 0, ForceMode.Force);
-        } else if(playerBody.position.x > 3.9f) {
+            _playerBody.AddForce (0.04f + nudgeBonus, 0, 0, ForceMode.Force);
+        } else if (_playerBody.position.x > 3.9f) {
             // If on the right edge
-             playerBody.AddForce(-0.04f - nudgeBonus, 0, 0, ForceMode.Force);
+            _playerBody.AddForce (-0.04f - nudgeBonus, 0, 0, ForceMode.Force);
         }
 
-        if(playerBody.position.z < 0.1f) {
+        if (_playerBody.position.z < 0.1f) {
             // If on the bottom edge
-             playerBody.AddForce(0, 0, 0.04f + nudgeBonus, ForceMode.Force);
-        } else if(playerBody.position.z > 3.9f) {
+            _playerBody.AddForce (0, 0, 0.04f + nudgeBonus, ForceMode.Force);
+        } else if (_playerBody.position.z > 3.9f) {
             // If on the top edge
-            playerBody.AddForce(0, 0, -0.04f - nudgeBonus, ForceMode.Force);
+            _playerBody.AddForce (0, 0, -0.04f - nudgeBonus, ForceMode.Force);
         }
-     }
- }
+    }
+
+    void checkIfHasFallen () {
+
+        if (this.transform.position.y < 0) {
+            if (this.name == "_player1") {
+                // JOSH INCREASE SCORE FOR PLAYER 2 HERE
+            } else {
+                // JOSH INCREASE SCORE FOR PLAYER 1 HERE
+            }
+            lvl.levelloader ();
+        }
+
+    }
+
+    public void setMovementBonus (float movementBonus) {
+        this.movementBonus = movementBonus;
+    }
+
+    public void setNudgeBonus (float nudgeBonus) {
+        this.nudgeBonus = nudgeBonus;
+    }
+
+    public void setFreeze (bool freeze) {
+        this.freeze = freeze;
+    }
+}
--- DropArena/Assets/Scripts/PowerupCollision.cs
+++ DropArena/Assets/Scripts/PowerupCollision.cs
@@ -3,175 +3,145 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PowerupCollision : MonoBehaviour
-{
+public class PowerupCollision : MonoBehaviour {
 
-    private float _powerUpDuration;
-    private int _currentPowerup;
-    private bool _isPoweredUp;
-  
-    Text powerup;
+  private float _powerUpDuration;
+  private int _currentPowerup;
+  private bool _isPoweredUp;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Start with no powerups
-        _isPoweredUp = false;
+  private Text powerup;
+
+  // Start is called before the first frame update
+  void Start () {
+    // Start with no powerups
+    _isPoweredUp = false;
+  }
+
+  // Update is called once per frame
+  void Update () {
+    if (Time.time >= _powerUpDuration + 5.0f && _isPoweredUp) {
+      _isPoweredUp = false;
+      resetPlayer ();
+      powerup.text = "";
     }
+  }
 
-    // Update is called once per frame
-    void Update()
-    {
-        if(Time.time >= _powerUpDuration + 5.0f && _isPoweredUp) {
-            _isPoweredUp = false;
-            resetPlayer();
-            powerup.text = "";
-        } 
-    }
+  void OnCollisionEnter (Collision collision) {
+    powerup = GameObject.Find ("Text").GetComponent<Text> ();
 
-    void OnCollisionEnter(Collision collision){   
-        powerup = GameObject.Find("Text").GetComponent<Text>();
+    // Check for collision with powerup   	
+    if (collision.gameObject.name == "_powerup" && _isPoweredUp == false) {
 
-        GameObject player1 = GameObject.Find("_player1");
+      _powerUpDuration = Time.time;
+      _isPoweredUp = true;
 
-            // Check for collision with powerup   	
-		 if (collision.gameObject.name == "_powerup" && _isPoweredUp == false)
-		{
+      // Get the type of powerup
+      _currentPowerup = getPowerupType ();
 
-            _powerUpDuration = Time.time;
-            _isPoweredUp = true;
+      // Destroy the powerup object
+      Destroy (collision.gameObject);
 
-            // Get the type of powerup
-            _currentPowerup = getPowerupType();
-            // Destroy the powerup object
-            Destroy(collision.gameObject);
+      switch (_currentPowerup) {
 
-            switch(_currentPowerup) {
-
-            case (int) InstantiatePowerups.PowerUps.sizePowerup:   
-                if(this.name == "_player1") {
-                  powerup.text="Player 1 Size PowerUp";
-                } else {
-                  powerup.text="Player 2 Size PowerUp";
-                  }     
-                  this.transform.localScale += new Vector3(0.5f, 0.5f, 0.5f);
-                break;
-
-
-            case (int) InstantiatePowerups.PowerUps.speedPowerup:       
-                  if(this.name == "_player1") {
-                    player1.GetComponent<Movement>().movementBonusPlayer1 = 1.5f;
-                    powerup.text="Player 1 Speed Power Up";
-                  } else {
-                    player1.GetComponent<Movement>().movementBonusPlayer2 = 1.5f;
-                    powerup.text="Player 2 Speed Power Up";
-                  }
-                  break;
-
-
-            case (int) InstantiatePowerups.PowerUps.massPowerup: 
-                  if(this.name == "_player1") {
-                  powerup.text="Player 1 Mass Power Up";  
-                  } else {
-                  powerup.text="Player 2 Mass Power Up";
-                  }  
-                  this.GetComponent<Rigidbody>().mass += 0.6f;  
-                  break;
-
-
-            case (int) InstantiatePowerups.PowerUps.nofallPowerup: 
-             if(this.name == "_player1") {
-                    player1.GetComponent<Movement>().nudgeBonusPlayer1 = 2f;
-                    powerup.text="Player 1 Force Power Up";
-                  } else {
-                    player1.GetComponent<Movement>().nudgeBonusPlayer2 = 2f;
-                    powerup.text="Player 2 Force Power Up";
-                  }
-                  break;
-
-
-            case (int) InstantiatePowerups.PowerUps.sizePowerdown: 
-            if(this.name == "_player1") {
-                    powerup.text="Player 1 Size Power Down";
-                  } else {
-                    powerup.text="Player 2 Size Power Down";
-                  }
-                  this.transform.localScale += new Vector3(-0.5f, -0.5f, -0.5f);
-                  break;
-
-
-            case (int) InstantiatePowerups.PowerUps.speedPowerdown:
-             if(this.name == "_player1") {
-                    player1.GetComponent<Movement>().movementBonusPlayer1 = 0.5f;
-                    powerup.text="Player 1 Speed Power Up";
-                  } else {
-                    player1.GetComponent<Movement>().movementBonusPlayer2 = 0.5f;
-                    powerup.text="Player 2 Speed Power Up";
-                  }
-                  break;
-
-
-            case (int) InstantiatePowerups.PowerUps.massPowerdown: 
-             if(this.name == "_player1") {           
-                  powerup.text="Player 1 Mass Power Down";
-                } else { 
-                  powerup.text="Player 2 Mass Power Down";
-                }
-                   this.GetComponent<Rigidbody>().mass -= 0.6f;  
-                break;
-
-
-            case (int) InstantiatePowerups.PowerUps.freezePowerDown: 
-            if(this.name == "_player1") {
-                    player1.GetComponent<Movement>().freezePlayer1 = true;
-                    powerup.text="Player 1 Freeze";
-                  } else {
-                    player1.GetComponent<Movement>().freezePlayer2 = true;
-                    powerup.text="Player 2 Freeze";
-                  }
-                  break;
-
+        case (int) InstantiatePowerups.PowerUps.sizePowerup:
+          if (this.name == "_player1") {
+            powerup.text = "Player 1 Size PowerUp";
+          } else {
+            powerup.text = "Player 2 Size PowerUp";
           }
-		}
+          this.transform.localScale += new Vector3 (0.5f, 0.5f, 0.5f);
+          break;
 
-        }
+        case (int) InstantiatePowerups.PowerUps.speedPowerup:
+          if (this.name == "_player1") {
+            powerup.text = "Player 1 Speed Power Up";
+          } else {
+            powerup.text = "Player 2 Speed Power Up";
+          }
+          this.GetComponent<Movement> ().setMovementBonus (1.5f);
+          break;
 
-        private int getPowerupType() {
-            // Retrieve script attached to player1
-        GameObject player1 = GameObject.Find("_player1");
-        InstantiatePowerups instantiatePowerupScript = player1.GetComponent<InstantiatePowerups>();
-        return instantiatePowerupScript.getPowerupType();
-        }
+        case (int) InstantiatePowerups.PowerUps.massPowerup:
+          if (this.name == "_player1") {
+            powerup.text = "Player 1 Mass Power Up";
+          } else {
+            powerup.text = "Player 2 Mass Power Up";
+          }
+          this.GetComponent<Rigidbody> ().mass += 0.6f;
+          break;
 
-        private void resetPlayer() {
-            // Reset the scale
-            this.transform.localScale = Vector3.one;
+        case (int) InstantiatePowerups.PowerUps.nofallPowerup:
+          if (this.name == "_player1") {
+            powerup.text = "Player 1 Force Power Up";
+          } else {
+            powerup.text = "Player 2 Force Power Up";
+          }
+          this.GetComponent<Movement> ().setNudgeBonus (2f);
+          break;
 
-            // Reset the speed
-            GameObject player1 = GameObject.Find("_player1");
-            if(this.name == "_player1") {
-                player1.GetComponent<Movement>().movementBonusPlayer1 = 1;
-            }
-            else  {
-                player1.GetComponent<Movement>().movementBonusPlayer2 = 1;
-            }
+        case (int) InstantiatePowerups.PowerUps.sizePowerdown:
+          if (this.name == "_player1") {
+            powerup.text = "Player 1 Size Power Down";
+          } else {
+            powerup.text = "Player 2 Size Power Down";
+          }
+          this.transform.localScale += new Vector3 (-0.5f, -0.5f, -0.5f);
+          break;
 
-            // Reset the nofall
-              if(this.name == "_player1") {
-                    player1.GetComponent<Movement>().nudgeBonusPlayer1 = 0;
-                  } else {
-                    player1.GetComponent<Movement>().nudgeBonusPlayer2 = 0;
-                  }
+        case (int) InstantiatePowerups.PowerUps.speedPowerdown:
+          if (this.name == "_player1") {
+            powerup.text = "Player 1 Speed Power Up";
+          } else {
+            powerup.text = "Player 2 Speed Power Up";
+          }
+          this.GetComponent<Movement> ().setMovementBonus (0.5f);
+          break;
 
-            // Reset the mass
-            this.GetComponent<Rigidbody>().mass = 1f;
+        case (int) InstantiatePowerups.PowerUps.massPowerdown:
+          if (this.name == "_player1") {
+            powerup.text = "Player 1 Mass Power Down";
+          } else {
+            powerup.text = "Player 2 Mass Power Down";
+          }
+          this.GetComponent<Rigidbody> ().mass -= 0.6f;
+          break;
 
-            // Reset the freeze
-                if(this.name == "_player1") {
-                    player1.GetComponent<Movement>().freezePlayer1 = false;
-                  } else {
-                    player1.GetComponent<Movement>().freezePlayer2 = false;
-                  }
-        }
-  	
+        case (int) InstantiatePowerups.PowerUps.freezePowerDown:
+          if (this.name == "_player1") {
+            powerup.text = "Player 1 Freeze";
+          } else {
+            powerup.text = "Player 2 Freeze";
+          }
+          this.GetComponent<Movement> ().setFreeze (true);
+          break;
+
+      }
+    }
+
+  }
+
+  private int getPowerupType () {
+    // Retrieve script attached to player1
+    GameObject player1 = GameObject.Find ("_player1");
+    InstantiatePowerups instantiatePowerupScript = player1.GetComponent<InstantiatePowerups> ();
+    return instantiatePowerupScript.getPowerupType ();
+  }
+
+  private void resetPlayer () {
+    // Reset the scale
+    this.transform.localScale = Vector3.one;
+
+    // Reset the speed
+    this.GetComponent<Movement> ().setMovementBonus (1);
+
+    // Reset the nofall
+    this.GetComponent<Movement> ().setNudgeBonus (0);
+
+    // Reset the mass
+    this.GetComponent<Rigidbody> ().mass = 1f;
+
+    // Reset the freeze
+    this.GetComponent<Movement> ().setFreeze (false);
+  }
+
 }
